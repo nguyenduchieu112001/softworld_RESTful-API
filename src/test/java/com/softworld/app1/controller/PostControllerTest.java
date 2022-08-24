1b4d161fe6e57eab2d47f7149680eddce26395e7
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2022-08-24 15:09:39 +0700

    update code ver 2

--- src/test/java/com/softworld/app1/controller/PostControllerTest.java
+++ src/test/java/com/softworld/app1/controller/PostControllerTest.java
@@ -2,17 +2,11 @@ package com.softworld.app1.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doCallRealMethod;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -39,7 +33,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.softworld.app1.controller.form.PostInput;
 import com.softworld.app1.model.Category;
+import com.softworld.app1.model.Category_Post;
 import com.softworld.app1.model.Post;
 import com.softworld.app1.service.CategoryPostServiceImpl;
 import com.softworld.app1.service.CategoryServiceImpl;
@@ -49,6 +45,7 @@ import com.softworld.app1.service.PostServiceImpl;
 @WebMvcTest(value = PostController.class)
 @WebAppConfiguration
 @RunWith(SpringRunner.class)
+@SuppressWarnings("deprecation")
 public class PostControllerTest {
 
 	@Autowired
@@ -66,9 +63,19 @@ public class PostControllerTest {
 	public void setup() {
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
+	
+	// truyen du lieu cho mockMvc
+	public static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	@Test
 	@DisplayName("Test findAll")
+	@WithMockUser(username = "sa", authorities = "ROLE_ADMIN")
 	public void assert_that_find_all_posts() throws Exception {
 		Post post_1 = new Post("ab", "ab", "2020-04-04", "2020-04-04");
 		post_1.setPostId(1L);
@@ -101,6 +108,7 @@ public class PostControllerTest {
 
 	@Test
 	@DisplayName("Test findById success")
+	@WithMockUser(username = "sa", authorities = "ROLE_ADMIN")
 	public void assert_that_get_post_by_id() throws Exception {
 		Post post = new Post("ab", "ab", "2020-04-04", "2020-04-04");
 		post.setPostId(1L);
@@ -135,17 +143,8 @@ public class PostControllerTest {
 		Assertions.assertTrue(json.get("title").getAsString().equals("abc"));
 	}
 
-	// truyen du lieu cho mockMvc
-	public static String asJsonString(final Object obj) {
-		try {
-			return new ObjectMapper().writeValueAsString(obj);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
 	@Test
-	@DisplayName("Test update Post")
+	@DisplayName("Test update Post by id")
 	@WithMockUser(username = "sa", authorities = "ROLE_ADMIN")
 	public void assert_that_post_is_update_successfully() throws Exception {
 		Post post = new Post("abcd", "abcd", "2020-12-12", "2020-12-12");
@@ -158,7 +157,7 @@ public class PostControllerTest {
 		when(postService.save(any(Post.class))).thenReturn(newPost);
 
 		MvcResult mvcResult = mockMvc
-				.perform(MockMvcRequestBuilders.put("/api/post/edit/{id}", post.getPostId()).content(asJsonString(post))
+				.perform(MockMvcRequestBuilders.put("/api/post/edit/{id}", post.getPostId()).content(asJsonString(newPost))
 						.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
 				.andDo(MockMvcResultHandlers.print()).andReturn();
 
@@ -174,18 +173,19 @@ public class PostControllerTest {
 		Post post = new Post("ab", "ab", "2020-04-04", "2020-04-04");
 		post.setPostId(1L);
 
-		when(postService.delete(any(long.class))).thenReturn(null);
+		when(postService.getById(any(Long.class))).thenReturn(post);
 
 		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.delete("/api/post/delete/{id}", post.getPostId()))
 				.andDo(MockMvcResultHandlers.print()).andReturn();
 
-		Assertions.assertEquals("", mvcResult.getResponse().getContentAsString());
+		JsonObject json = new JsonParser().parse(mvcResult.getResponse().getContentAsString()).getAsJsonObject();
+		Assertions.assertTrue(json.get("message").getAsString().equals("Delete completed"));
 	}
 
 	@Test
 	@DisplayName("Test get Post by id with categoryName and categoryID")
 	@WithMockUser(username = "sa", authorities = "ROLE_ADMIN")
-	public void get_post_by_id_with_categoryName_categoryID() throws Exception {
+	public void assert_that_get_post_by_id_with_categoryName_categoryID() throws Exception {
 		Post post = new Post("abc", "abc", "2020-09-09", "2020-09-09");
 		post.setPostId(1L);
 
@@ -200,54 +200,143 @@ public class PostControllerTest {
 
 		when(postService.getPostById(anyLong())).thenReturn(post);
 		when(cpService.getCategoryIDFromCategoryPost(any(Long.class))).thenReturn(listCategoryID);
-		when(categoryService.getNameById(anyLong())).thenReturn(category_1.getCategoryName()).thenReturn(category_2.getCategoryName());
+		when(categoryService.getNameById(anyLong())).thenReturn(category_1.getCategoryName())
+				.thenReturn(category_2.getCategoryName());
 
 		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/get/post/{id}", post.getPostId()))
 				.andDo(MockMvcResultHandlers.print()).andReturn();
-		
+
 		JsonObject json = new JsonParser().parse(mvcResult.getResponse().getContentAsString()).getAsJsonObject();
 		Assertions.assertTrue(json.get("title").getAsString().equals("abc"));
-		
-		 JsonArray jsonArray = json.getAsJsonArray("categories"); 
-		 ArrayList<Object> listdata = new ArrayList<Object>();  
-		 if(jsonArray != null)
-			 for(int i=0; i<jsonArray.size(); i++)
-				 listdata.add(jsonArray.get(i));
-		 JsonObject jsonCategory = new JsonParser().parse(listdata.get(0).toString()).getAsJsonObject();
+
+		JsonArray jsonArray = json.getAsJsonArray("categories");
+		ArrayList<Object> listdata = new ArrayList<Object>();
+		if (jsonArray != null)
+			for (int i = 0; i < jsonArray.size(); i++)
+				listdata.add(jsonArray.get(i));
+		JsonObject jsonCategory = new JsonParser().parse(listdata.get(0).toString()).getAsJsonObject();
 		Assertions.assertTrue(jsonCategory.get("categoryName").getAsString().equals("Ashe"));
 	}
-	
-//	@Test
-//	@DisplayName("Test create Post with CategoryName and CategoryId")
-//	@WithMockUser(username = "sa", authorities = "ROLE_ADMIN")
-//	public void create_post_with_categoryIDs() throws Exception {
-//		Post post = new Post("abc", "abc", null, null);
-//		post.setPostId(1L);
-//		
-//		List<Long> listCategoryIDs = new ArrayList<>();
-//		listCategoryIDs.add(1L);
-//		listCategoryIDs.add(2L);
-//		
-//		CategoryPostServiceImpl cpServiceImpl = mock(CategoryPostServiceImpl.class);
-//		
-//		when(postService.save(any(Post.class))).thenReturn(post);
-//		doNothing().when(cpServiceImpl).insertCategoryPost(any(Long.class), any(Long.class));
-//		
-//		cpService.insertCategoryPost(1L, 1L);
-//		
-//		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/api/create/post"))
-//									.andDo(MockMvcResultHandlers.print())
-//									.andReturn();
-//		
-//	}
+
+	@Test
+	@DisplayName("Test create Post with CategoryId")
+	@WithMockUser(username = "sa", authorities = "ROLE_ADMIN")
+	public void assert_that_create_post_with_categoryIDs() throws Exception {
+
+		List<Long> listCategoryIDs = new ArrayList<>();
+		listCategoryIDs.add(1L);
+		listCategoryIDs.add(2L);
+
+		MvcResult mvcResult = mockMvc
+				.perform(MockMvcRequestBuilders.post("/api/create/post")
+						.content(asJsonString(new PostInput("ab", "ab", listCategoryIDs)))
+						.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+				.andDo(MockMvcResultHandlers.print()).andReturn();
+
+		JsonObject json = new JsonParser().parse(mvcResult.getResponse().getContentAsString()).getAsJsonObject();
+		Assertions.assertTrue(json.get("message").getAsString().equals("Create completed"));
+	}
+
+	@Test
+	@DisplayName("Test update Post by id with categoryID and categoryName using PostInput")
+	@WithMockUser(username = "sa", authorities = "ROLE_ADMIN")
+	public void assert_that_update_post_by_id_witn_categoryID_and_categoryName() throws Exception {
+
+		Post post = new Post("ab", "ab", null, null);
+		post.setPostId(1L);
+
+		Category category_1 = new Category("Ashe", null, null);
+		category_1.setCategoryID(1L);
+		Category category_2 = new Category("Udyr", null, null);
+		category_2.setCategoryID(2L);
+		Category_Post category_post_1 = new Category_Post(category_1.getCategoryID(), post.getPostId());
+		Category_Post category_post_2 = new Category_Post(category_2.getCategoryID(), post.getPostId());
+
+		Category category_3 = new Category("Luffy", null, null);
+		category_3.setCategoryID(3L);
+		Category category_4 = new Category("Ulta", null, null);
+		category_4.setCategoryID(4L);
+		List<Long> listCategoryIDs = new ArrayList<>();
+		listCategoryIDs.add(3L);
+		listCategoryIDs.add(4L);
+
+		when(postService.getPostById(any(Long.class))).thenReturn(post);
+		when(cpService.getCategoryIDFromCategoryPost(any(Long.class))).thenReturn(listCategoryIDs);
+		when(categoryService.getNameById(anyLong())).thenReturn(category_3.getCategoryName())
+				.thenReturn(category_4.getCategoryName());
+
+		MvcResult mvcResult = mockMvc
+				.perform(MockMvcRequestBuilders.put("/api/update/post/{id}", post.getPostId())
+						.content(asJsonString(new PostInput("ba", "ba", listCategoryIDs)))
+						.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+				.andDo(MockMvcResultHandlers.print()).andReturn();
+
+		JsonObject json = new JsonParser().parse(mvcResult.getResponse().getContentAsString()).getAsJsonObject();
+		Assertions.assertTrue(json.get("title").getAsString().equals("ba"));
+
+		JsonArray jsonArray = json.getAsJsonArray("categories");
+		ArrayList<Object> listdata = new ArrayList<Object>();
+		if (jsonArray != null)
+			for (int i = 0; i < jsonArray.size(); i++)
+				listdata.add(jsonArray.get(i));
+		JsonObject jsonCategory_1 = new JsonParser().parse(listdata.get(0).toString()).getAsJsonObject();
+		Assertions.assertTrue(jsonCategory_1.get("categoryName").getAsString().equals("Luffy"));
+
+		JsonObject jsonCategory_2 = new JsonParser().parse(listdata.get(1).toString()).getAsJsonObject();
+		Assertions.assertTrue(jsonCategory_2.get("categoryName").getAsString().equals("Ulta"));
+	}
+
+	@Test
+	@DisplayName("Test delete Post by id with categoryID and categoryName")
+	@WithMockUser(username = "sa", authorities = "ROLE_ADMIN")
+	public void assert_that_delete_post_by_id_with_categoryId_and_categoryName() throws Exception {
+		Post post = new Post();
+		post.setPostId(1L);
+
+		Category category_1 = new Category();
+		category_1.setCategoryID(1L);
+		Category category_2 = new Category();
+		category_2.setCategoryID(2L);
+//		Category_Post category_post_1 = new Category_Post(category_1.getCategoryID(), post.getPostId());
+//		Category_Post category_post_2 = new Category_Post(category_2.getCategoryID(), post.getPostId());
+
+		List<Long> listCategoryIDs = new ArrayList<>();
+		listCategoryIDs.add(category_1.getCategoryID());
+		listCategoryIDs.add(category_2.getCategoryID());
+
+		when(cpService.getCategoryIDFromCategoryPost(any(Long.class))).thenReturn(listCategoryIDs);
+
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.delete("/api/delete/post/{id}", 1L))
+				.andDo(MockMvcResultHandlers.print()).andReturn();
+
+		JsonObject json = new JsonParser().parse(mvcResult.getResponse().getContentAsString()).getAsJsonObject();
+		Assertions.assertTrue(json.get("message").getAsString().equals("Delete completed"));
+
+	}
 
 	@Test
 	@DisplayName("Test upload image")
 	@WithMockUser(username = "sa", authorities = "ROLE_ADMIN")
 	public void assert_that_upload_image() throws Exception {
 		MockMultipartFile file = new MockMultipartFile("file", "zoro.png", "image/png", "image".getBytes());
+
 		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.multipart("/api/upload/image").file(file))
 				.andDo(MockMvcResultHandlers.print()).andReturn();
-		
+
+		JsonObject json = new JsonParser().parse(mvcResult.getResponse().getContentAsString()).getAsJsonObject();
+		Assertions.assertTrue(json.get("message").getAsString().equals("http://localhost/api/downloadFile/zoro.png"));
+	}
+
+	@Test
+	@DisplayName("Test download image")
+	@WithMockUser(username = "sa", authorities = "ROLE_ADMIN")
+	public void assert_that_download_image_not_exist() throws Exception {
+		String fileName = "zoro.jpg";
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/downloadFile/{file:.+}", fileName))
+				.andDo(MockMvcResultHandlers.print()).andReturn();
+
+		JsonObject json = new JsonParser().parse(mvcResult.getResponse().getContentAsString()).getAsJsonObject();
+		Assertions.assertTrue(json.get("message").getAsString().equals("Your image file does not exist"));
+
 	}
 }
---
+++ src/test/java/com/softworld/app1/controller/UserControllerTest.java
@@ -0,0 +1,166 @@
+package com.softworld.app1.controller;
+
+import static org.mockito.Mockito.when;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import static org.mockito.ArgumentMatchers.any;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.softworld.app1.controller.form.UserInput;
+import com.softworld.app1.controller.form.UserRole;
+import com.softworld.app1.model.User;
+import com.softworld.app1.service.EmailSenderService;
+import com.softworld.app1.service.RoleServiceImpl;
+import com.softworld.app1.service.UserService;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(value = UserController.class)
+@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SuppressWarnings("deprecation")
+public class UserControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	@MockBean
+	private UserService userService;
+	@MockBean
+	private EmailSenderService mailSenderService;
+	@MockBean
+	private RoleServiceImpl roleService;
+
+	@BeforeEach
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	// truyen du lieu cho mockMvc
+	public static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	@DisplayName("Test create new User")
+	public void assert_that_user_is_created_successfully() throws Exception {
+		User user = new User("user", "nguyen van a", DigestUtils.sha1Hex("12345678"), null, "nguyenvana@gmail.com");
+		user.setUserID(1L);
+
+		when(userService.save(any(User.class))).thenReturn(user);
+
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/api/user/create")
+				.content(asJsonString(user)).contentType(MediaType.APPLICATION_JSON))
+				.andDo(MockMvcResultHandlers.print()).andReturn();
+
+		JsonObject json = new JsonParser().parse(mvcResult.getResponse().getContentAsString()).getAsJsonObject();
+		Assertions.assertTrue(json.get("userName").getAsString().equals("user"));
+	}
+
+	@Test
+	@DisplayName("Test update user by id")
+	public void assert_that_user_id_updated_successfully() throws Exception {
+		User oldUser = new User("user", "nguyen van a", DigestUtils.sha1Hex("12345678"), null, "nguyenvana@gmail.com");
+		oldUser.setUserID(1L);
+
+		User newUser = new User("admin", "tran van b", DigestUtils.sha1Hex("12345678"), null, "tranvanb@gmail.com");
+		newUser.setUserID(1L);
+
+		when(userService.getById(any(Long.class))).thenReturn(oldUser);
+		when(userService.save(any(User.class))).thenReturn(newUser);
+
+		MvcResult mvcResult = mockMvc
+				.perform(MockMvcRequestBuilders.put("/api/user/edit/{id}", oldUser.getUserID())
+						.content(asJsonString(newUser)).contentType(MediaType.APPLICATION_JSON))
+				.andDo(MockMvcResultHandlers.print()).andReturn();
+
+		JsonObject json = new JsonParser().parse(mvcResult.getResponse().getContentAsString()).getAsJsonObject();
+		Assertions.assertTrue(json.get("userName").getAsString().equals("admin"));
+	}
+
+	@Test
+	@DisplayName("Test delete user by id")
+	public void assert_that_user_is_deleted_successfully() throws Exception {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		Date now = new Date();
+
+		User user = new User("user", "nguyen van a", DigestUtils.sha1Hex("12345678"), null, "nguyenvana@gmail.com");
+		user.setUserID(1L);
+
+		when(userService.getById(any(Long.class))).thenReturn(user);
+		when(userService.save(any(User.class))).thenReturn(user);
+
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.put("/api/user/delete/{id}", user.getUserID())
+				.contentType(MediaType.APPLICATION_JSON)).andDo(MockMvcResultHandlers.print()).andReturn();
+
+		JsonObject json = new JsonParser().parse(mvcResult.getResponse().getContentAsString()).getAsJsonObject();
+		Assertions.assertTrue(json.get("deleteAt").getAsString().equals(formatter.format(now)));
+	}
+
+	@Test
+	@DisplayName("Test login")
+	public void asssert_that_login_successful() throws Exception {
+		User user = new User("user", "nguyen van a", DigestUtils.sha1Hex("12345678"), null, "nguyenvana@gmail.com");
+		user.setUserID(1L);
+		UserRole userRole = new UserRole("user", "user");
+
+		when(userService.getUserName(any(String.class))).thenReturn(user);
+		when(roleService.getRoleName(any(String.class))).thenReturn(userRole.getUsername());
+
+		MvcResult mvcResult = mockMvc
+				.perform(MockMvcRequestBuilders.post("/api/login")
+						.content(asJsonString(new UserInput("user", "12345678")))
+						.contentType(MediaType.APPLICATION_JSON_UTF8).accept(MediaType.APPLICATION_JSON_UTF8))
+				.andDo(MockMvcResultHandlers.print()).andReturn();
+
+		JsonObject json = new JsonParser().parse(mvcResult.getResponse().getContentAsString()).getAsJsonObject();
+		Assertions.assertTrue(json.get("error").getAsString().equals("OK"));
+	}
+
+	@Test
+	@DisplayName("Test convert JWT to json")
+	public void assert_that_convert_jwt_to_json() throws Exception {
+		String token = "eyJhbGciOiJIUzUxMiJ9.eyJmdWxsTmFtZSI6Ik5ndXllbiBNaW5oIE4iLCJyb2xlIjoiUk9MRV9BRE1JTiIsInN1YiI6InNhIiwiaWF0IjoxNjYxMzI2NTUzLCJleHAiOjg4MDYxMzI2NTUzfQ.Tln9VX5IJC7WDTqlvgCYnv5QbiqwwrgmGuHPBSeIDn5fKMYpCiMPfkjF3xW8CGF9Qmz_hs-L07uhgb1ThPuUyg";
+		
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/convert")
+				.param("token", token)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andDo(MockMvcResultHandlers.print())
+				.andReturn();
+		
+		JsonObject json = new JsonParser().parse(mvcResult.getResponse().getContentAsString()).getAsJsonObject();
+		Assertions.assertTrue(json.get("fullName").getAsString().equals("Nguyen Minh N"));
+	}
+	
+	
+}
